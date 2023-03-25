7ff31115ca14c33cf75e3745985b28825883732f
Matheus Felisberto <matheus.felisberto@hotmail.com>
2023-03-25 22:04:54 +0000

    [FLINK-26088] add: elasticsearch writer

---
+++ flink-connector-elasticsearch8/src/main/java/org/apache/flink/connector/elasticsearch/sink/Elasticsearch8Writer.java
@@ -0,0 +1,116 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+package org.apache.flink.connector.elasticsearch.sink;
+
+import co.elastic.clients.elasticsearch.ElasticsearchAsyncClient;
+import co.elastic.clients.elasticsearch.core.BulkRequest;
+import co.elastic.clients.elasticsearch.core.bulk.BulkOperation;
+import org.apache.flink.api.connector.sink2.Sink;
+import org.apache.flink.connector.base.sink.writer.AsyncSinkWriter;
+import org.apache.flink.connector.base.sink.writer.BufferedRequestState;
+import org.apache.flink.connector.base.sink.writer.ElementConverter;
+import org.apache.http.HttpHost;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+public class Elasticsearch8Writer<InputT> extends AsyncSinkWriter<InputT, Operation> {
+    private static final Logger LOG = LoggerFactory.getLogger(ElasticsearchWriter.class);
+
+    private final ElasticsearchAsyncClient esClient;
+
+    public Elasticsearch8Writer(
+        ElementConverter<InputT, Operation> elementConverter,
+        Sink.InitContext context,
+        int maxBatchSize,
+        int maxInFlightRequests,
+        int maxBufferedRequests,
+        long maxBatchSizeInBytes,
+        long maxTimeInBufferMS,
+        long maxRecordSizeInBytes,
+        String username,
+        String password,
+        HttpHost[] httpHosts,
+        Collection<BufferedRequestState<Operation>> state
+    ) {
+        super(
+            elementConverter,
+            context,
+            maxBatchSize,
+            maxInFlightRequests,
+            maxBufferedRequests,
+            maxBatchSizeInBytes,
+            maxTimeInBufferMS,
+            maxRecordSizeInBytes,
+            state
+        );
+
+        this.esClient = new NetworkConfigFactory(httpHosts, username, password).create();
+    }
+
+    @Override
+    protected void submitRequestEntries(List<Operation> requestEntries, Consumer<List<Operation>> requestResult) {
+        LOG.info("submitRequestEntries with {} items", requestEntries.size());
+
+        BulkRequest.Builder bulkRequest = new BulkRequest.Builder();
+
+        ArrayList<BulkOperation> bulkOperations = requestEntries.stream()
+            .map(Operation::getBulkOperation)
+            .collect(Collectors.toCollection(ArrayList::new));
+
+        /**
+         * Unfortunately, Elasticsearch Client doesn't support
+         * retries yet nor does a way to see a failed operation;
+         *
+         * @see https://github.com/elastic/elasticsearch-java/issues/356
+         */
+        esClient.bulk(bulkRequest.operations(bulkOperations).build())
+            .whenComplete((response, exception) -> {
+                // All batch have failed
+                if (exception != null) {
+                    requestResult.accept(requestEntries);
+                    LOG.error("The batch has failed", exception);
+
+                    // A few batch items have failed
+                } else if (response.errors()) {
+                    response.items().stream()
+                        .filter(item -> item.error() != null)
+                        .forEach(item -> LOG.error("Failed item: {}", item));
+
+                    requestResult.accept(Collections.emptyList());
+
+                    // The whole batch was written successfully
+                } else {
+                    LOG.info("Bulk succeed with {} items in {}", requestEntries.size(), response.took());
+                    requestResult.accept(Collections.emptyList());
+                }
+            });
+    }
+
+    @Override
+    protected long getSizeInBytes(Operation requestEntry) {
+        return new OperationSerializer().size(requestEntry);
+    }
+}
