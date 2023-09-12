78ca8a62c0ce1593b266d31c716981c3ecf68da7
Matheus Felisberto <matheus.felisberto@hotmail.com>
2023-09-13 00:41:29 +0100

    [FLINK-26088] test: implementing OperationSerializer tests

--- flink-connector-elasticsearch8/src/test/java/org/apache/flink/connector/elasticsearch/sink/OperationSerializerTest.java
+++ flink-connector-elasticsearch8/src/test/java/org/apache/flink/connector/elasticsearch/sink/OperationSerializerTest.java
@@ -1,42 +1,89 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
 package org.apache.flink.connector.elasticsearch.sink;
 
+import co.elastic.clients.elasticsearch.core.bulk.BulkOperationVariant;
+import co.elastic.clients.elasticsearch.core.bulk.DeleteOperation;
 import co.elastic.clients.elasticsearch.core.bulk.IndexOperation;
-
 import co.elastic.clients.elasticsearch.core.bulk.UpdateOperation;
+import org.assertj.core.api.recursive.comparison.RecursiveComparisonConfiguration;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
 
-import org.apache.flink.api.common.serialization.SimpleStringSchema;
-import org.apache.flink.connector.base.sink.writer.BufferedRequestState;
-import org.apache.flink.connector.base.sink.writer.ElementConverter;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Test;
-import java.io.IOException;
-
-import static org.apache.flink.connector.elasticsearch.sink.AsyncSinkWriterTestUtils.assertThatBufferStatesAreEqual;
-import static org.apache.flink.connector.elasticsearch.sink.AsyncSinkWriterTestUtils.getTestState;
-
+/**
+ * OperationSerializer tests.
+ *
+ */
 public class OperationSerializerTest {
+    @ParameterizedTest
+    @MethodSource("operations")
+    public void testSerializeAndDeserialize(BulkOperationVariant operationVariant) {
+        Operation expectedState = new Operation(
+            operationVariant,
+            new AtomicInteger(3)
+        );
 
-    private static final Elasticsearch8SinkBuilder.OperationConverter<Elasticsearch8SinkTest.DummyData> ELEMENT_CONVERTER =
-        new Elasticsearch8SinkBuilder.OperationConverter<>((element, context) ->
-            new UpdateOperation
-                .Builder<Elasticsearch8SinkTest.DummyData, Elasticsearch8SinkTest.DummyData>()
-                .index("test")
-                .id(element.getId())
-                .action(op -> op.doc(element).docAsUpsert(true))
-                .retryOnConflict(3)
-                .build(), 3);
+        final ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        final DataOutputStream out = new DataOutputStream(bytes);
 
-    @Test
-    public void testSerializeAndDeserialize() throws IOException {
-        BufferedRequestState<Operation> expectedState =
-            getTestState(ELEMENT_CONVERTER, this::getRequestSize);
+        new OperationSerializer().serialize(expectedState, out);
 
-        Elasticsearch8SinkSerializer serializer = new Elasticsearch8SinkSerializer();
-        BufferedRequestState<Operation> actualState =
-                serializer.deserialize(1, serializer.serialize(expectedState));
-        assertThatBufferStatesAreEqual(actualState, expectedState);
+        final ByteArrayInputStream inputBytes = new ByteArrayInputStream(bytes.toByteArray());
+        final DataInputStream in = new DataInputStream(inputBytes);
+
+        Operation actualState = new OperationSerializer().deserialize(getRequestSize(expectedState), in);
+
+        assertThat(actualState.getBulkOperationVariant())
+            .usingRecursiveComparison(RecursiveComparisonConfiguration.builder().build())
+            .isEqualTo(expectedState.getBulkOperationVariant());
+    }
+
+    private static Stream<Arguments> operations() {
+        return Stream.of(
+            Arguments.of(new DeleteOperation.Builder().id("delete").build()),
+            Arguments.of(
+                new IndexOperation.Builder<>()
+                    .id("index")
+                    .index("testing")
+                    .document(new Object())
+                    .build()
+            ),
+            Arguments.of(new UpdateOperation.Builder<>()
+                    .id("update")
+                    .action(a -> a.doc(new Object()))
+                    .index("testing")
+                    .build()
+            )
+        );
     }
 
     private int getRequestSize(Operation requestEntry) {
