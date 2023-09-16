e79481755e37fcebaa7bcad8e3cb9d63de6d9811
Matheus Felisberto <matheus.felisberto@hotmail.com>
2023-09-16 12:52:45 +0100

    [FLINK-26088] fix: ElasticsearchSinkBaseITCase container configs

--- flink-connector-elasticsearch8/src/test/java/org/apache/flink/connector/elasticsearch/sink/ElasticsearchSinkBaseITCase.java
+++ flink-connector-elasticsearch8/src/test/java/org/apache/flink/connector/elasticsearch/sink/ElasticsearchSinkBaseITCase.java
@@ -27,12 +27,17 @@ import org.apache.http.util.EntityUtils;
 import org.elasticsearch.client.Request;
 import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy;
 import org.testcontainers.elasticsearch.ElasticsearchContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 import java.io.IOException;
+import java.time.Duration;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.Assert.assertEquals;
@@ -40,11 +45,13 @@ import static org.junit.Assert.assertEquals;
 /** Base Integration tests class. */
 @Testcontainers
 public class ElasticsearchSinkBaseITCase {
+    protected static final Logger LOG = LoggerFactory.getLogger(ElasticsearchSinkBaseITCase.class);
+
     public RestClient client;
 
     public ElasticsearchClient esClient;
 
-    public static final String ELASTICSEARCH_VERSION = "8.0.0";
+    public static final String ELASTICSEARCH_VERSION = "8.9.1";
 
     public static final DockerImageName ELASTICSEARCH_IMAGE = DockerImageName
         .parse("docker.elastic.co/elasticsearch/elasticsearch")
@@ -55,15 +62,19 @@ public class ElasticsearchSinkBaseITCase {
 
     public static ElasticsearchContainer createElasticsearchContainer() {
         try (ElasticsearchContainer container = new ElasticsearchContainer(ELASTICSEARCH_IMAGE)
-            .withEnv("xpack.security.enabled", "false")
-            .withExposedPorts(9400)
+            .withEnv("xpack.security.enabled", "false");
         ) {
+            container
+                .withEnv("ES_JAVA_OPTS", "-Xms2g -Xmx2g")
+                .withEnv("logger.org.elasticsearch", "DEBUG")
+                .withLogConsumer(new Slf4jLogConsumer(LOG));
+
             return container;
         }
     }
 
     public RestClient getRestClient() {
-        return RestClient.builder(new HttpHost(ES_CONTAINER.getHost(), 9400))
+        return RestClient.builder(new HttpHost(ES_CONTAINER.getHost(), ES_CONTAINER.getFirstMappedPort()))
             .setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder)
             .build();
     }
